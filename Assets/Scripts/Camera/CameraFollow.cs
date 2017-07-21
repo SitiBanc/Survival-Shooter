4cffd6b5afce026a6b3f441ff8dd99256256b8bf
Hsin-Yi Chen <hychen.iim05g@g2.nctu.edu.tw>
2017-07-21 17:26:32 +0800

    3. Camera Setup
    Setup camera (angle, position and projection type) and making it to follow player
    Add player object to prefabs

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;    // Our following target's transform
+    public float smoothing;     // Smooth the camera following speed
+
+    private Vector3 offset;     // The initial distance between camera and target
+
+    // Use this for initialization
+    void Start() {
+        // Get initial offset value
+        offset = transform.position - target.position;
+    }
+
+    void FixedUpdate() {
+        // The target position our camera is going to
+        Vector3 targetCamPos = offset + target.position;
+        // Smoothly move our camera to its target position using Vector3.Lerp()（線性插值）
+        // Smoothing * Time.deltaTime cause we want to move our camera smooth and slow (per second speed)
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}
