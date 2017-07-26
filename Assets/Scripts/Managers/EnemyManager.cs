ff619fa90cdab2448fcae6d57976c62f6eef2df2
Hsin-Yi Chen <hychen.iim05g@g2.nctu.edu.tw>
2017-07-26 17:05:07 +0800

    9. Spawning Enemies
    Create and add Zombear to prefabs
    Create and add Hellephant to prefabs
    Add EnemyManager and EnemyManager script
    Set every EnemyManager scipts to properly spawn the assigned enemy

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -10,7 +10,8 @@ public class EnemyManager : MonoBehaviour
 
     void Start ()
     {
-        InvokeRepeating ("Spawn", spawnTime, spawnTime);
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
     }
 
 
@@ -18,9 +19,11 @@ public class EnemyManager : MonoBehaviour
     {
         if(playerHealth.currentHealth <= 0f)
         {
+            // ... exit the function.
             return;
         }
 
+        // Randomly select a spawn point
         int spawnPointIndex = Random.Range (0, spawnPoints.Length);
 
         Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
