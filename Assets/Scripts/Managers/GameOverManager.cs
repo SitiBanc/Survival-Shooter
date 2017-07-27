3a72e20e1a3ac30a3efc506749a7f528a95aa316
Hsin-Yi Chen <hychen.iim05g@g2.nctu.edu.tw>
2017-07-27 11:14:52 +0800

    10. Game Over
    Add GamOver Image and GameOverText
    Add Fade-in and Bounce animation to HUDCanvas
    Add GameOverManager to restart the game

--- Assets/Scripts/Managers/GameOverManager.cs
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -3,9 +3,11 @@
 public class GameOverManager : MonoBehaviour
 {
     public PlayerHealth playerHealth;
+    public float restartDelay = 5f;         // Time to wait before restarting the level
 
 
     Animator anim;
+    float restartTimer;                     // Timer to count up to restarting the level
 
 
     void Awake()
@@ -19,6 +21,14 @@ public class GameOverManager : MonoBehaviour
         if (playerHealth.currentHealth <= 0)
         {
             anim.SetTrigger("GameOver");
+            // .. increment a timer to count up to restarting.
+            restartTimer += Time.deltaTime;
+
+            // .. if it reaches the restart delay...
+            if (restartTimer >= restartDelay)
+            {
+                playerHealth.RestartLevel();
+            }
         }
     }
 }
